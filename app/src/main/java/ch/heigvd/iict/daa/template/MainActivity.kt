c396be504851639d24b95e2dea469c61dc8edee8
SNSantos <stephane.nascimentosantos@heig-vd.ch>
2024-09-19 16:30:57 +0200

    Vertical mode dynamic?

--- app/src/main/java/ch/heigvd/iict/daa/template/MainActivity.kt
+++ app/src/main/java/ch/heigvd/iict/daa/template/MainActivity.kt
@@ -1,11 +1,27 @@
 package ch.heigvd.iict.daa.template
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_linear)
+
+        // Get a reference to the LinearLayout
+        val mainLayout = findViewById<LinearLayout>(R.id.main_layout)
+
+        // Check the current orientation
+        val orientation = resources.configuration.orientation
+
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            // Set LinearLayout to horizontal in landscape mode
+            mainLayout.orientation = LinearLayout.HORIZONTAL
+        } else {
+            // Set LinearLayout to vertical in portrait mode
+            mainLayout.orientation = LinearLayout.VERTICAL
+        }
     }
 }
